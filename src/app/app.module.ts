07ab2e3ee730f81947847cdd3022739837cec3ec
mateen <mateenforwork>
2018-08-28 06:22:03 +0530

    angualr skeleton with bootstrap4 and material angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [
+      CUSTOM_ELEMENTS_SCHEMA
+  ]
+})
+export class AppModule { }
